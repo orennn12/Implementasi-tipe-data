cab73f676a0488719e671897c60f5960ba726062
rizsatrio <satrios.playgame@gmail.com>
2025-06-02 23:15:32 +0800

    2. Mendeklarasi Stack

--- c030324107stack.cpp
+++ c030324107stack.cpp
@@ -2,5 +2,13 @@
 #include <conio.h>
 using namespace std;
 
+const int MAXSTACK = 100;
+
+typedef int ItemType;
+typedef struct {
+    ItemType Item[MAXSTACK];
+    int Count;
+} Stack;
+
 int main(){
 }
