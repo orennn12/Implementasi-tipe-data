21969546a45a2110abcb031f0e010878091c51d5
rizsatrio <satrios.playgame@gmail.com>
2025-06-02 22:19:29 +0800

    1. Program Dimulai

---
+++ c030324107struct.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+struct mahasiswa{
+    string nim;
+    string nama;
+    string alamat;
+    float ipk;
+};
+
+int main(){
+    mahasiswa rio;
+    return 0;
+}
